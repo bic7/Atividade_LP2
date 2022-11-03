7314793570e870205b1d48712fc1852a135f799c
bic7 <101570483+bic7@users.noreply.github.com>
2022-11-02 21:30:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ print(Style.BRIGHT + 'enfatizar o texto\n')
 
 print(Style.RESET_ALL)
 print('Voltando ao normal...')
+
+"""
+
+As constantes de formatação disponíveis são:
+Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Style: DIM, NORMAL, BRIGHT, RESET_ALL
+  
+Style.RESET_ALL 
+redefine o primeiro plano, o plano de fundo e o brilho. 
+O Colorama realizará essa reinicialização automaticamente na saída do programa.
+
+"""
